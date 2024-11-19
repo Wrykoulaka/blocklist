9c7a873b1fcd8c78c72c14fa34fd900e6acc50c9
Jose Espinosa <wrykoulaka@gmail.com>
2024-11-19 01:51:44 +0100

    Update list

--- skynet/update.sh
+++ skynet/update.sh
@@ -1,4 +1,4 @@
-wget https://raw.githubusercontent.com/Adamm00/IPSet_ASUS/refs/heads/master/filter.list \
+wget -O filter.list https://raw.githubusercontent.com/Adamm00/IPSet_ASUS/refs/heads/master/filter.list \
 && echo "\nhttps://iplists.firehol.org/files/firehol_abusers_1d.netset" >> filter.list \
 && git add . \
 && git commit -m "Update list" \
