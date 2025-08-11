53d4c6e4397d5e607481759e923ed8dd90f508ed
Jose Espinosa <wrykoulaka@gmail.com>
2025-08-11 12:56:05 +0200

    Update only in github

--- skynet/update.sh
+++ skynet/update.sh
@@ -1,5 +1,2 @@
 wget -O skynet.list https://raw.githubusercontent.com/Adamm00/IPSet_ASUS/refs/heads/master/filter.list \
-&& paste -s -d "\n" skynet.list custom.list > filter.list \
-&& git add . \
-&& git commit -m "Update list" \
-&& git push 
+&& paste -s -d "\n" skynet.list custom.list > filter.list
